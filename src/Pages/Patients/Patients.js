b0064623d17980547dcf1e164168a7b6fbb8acee
heshankavinda <2017cs083@stu.ucsc.cmb.ac.lk>
2024-01-25 19:43:35 +0530

    included relevant things into the css files and the component

---
+++ src/Pages/Patients/Patients.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./PatientsStyles.module.css";
+
+function Patients(){
+
+    return (
+        <div className={classes.section}>
+            <p className={classes.p}>hello</p>
+        </div>
+    )
+}
+
+export default Patients;
